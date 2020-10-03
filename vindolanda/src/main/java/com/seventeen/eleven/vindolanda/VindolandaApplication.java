6f5ba16f3ff7ce21f445159423cfeac86a200256
EmaCorrea <emanuel.correa@upr.edu>
2020-10-03 16:24:35 -0400

    Add Spring app

---
+++ vindolanda/src/main/java/com/seventeen/eleven/vindolanda/VindolandaApplication.java
@@ -0,0 +1,13 @@
+package com.seventeen.eleven.vindolanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VindolandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VindolandaApplication.class, args);
+	}
+
+}
---
+++ vindolanda/src/test/java/com/seventeen/eleven/vindolanda/VindolandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seventeen.eleven.vindolanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VindolandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
